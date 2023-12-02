2072c844776cffdf867f31dda693b6ad27c382f3
s1n7ax <srineshnisala@gmail.com>
2023-12-02 20:23:05 +0530

    feat: use lazy to install test dependencies

---
+++ tests/before-test.lua
@@ -0,0 +1,3 @@
+local temp_path = './.test_plugins'
+
+vim.opt.rtp:append(temp_path .. '/plenary.nvim')
--- tests/test-config.lua
+++
@@ -1,5 +0,0 @@
-local temp_path = './.test_plugins'
-
-vim.opt.rtp:append(temp_path .. '/plenary.nvim')
--- vim.opt.rtp:append(temp_path .. '/nvim-lspconfig')
--- vim.opt.rtp:append(temp_path .. '/mason.nvim')
