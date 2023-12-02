8667bd3ea2c92a1655b117200bc4800e565ecc39
s1n7ax <srineshnisala@gmail.com>
2023-12-03 00:54:42 +0530

    feat: add enrich config to adapter
    * some other config was moved from java-core to java-dap

---
+++ lua/java-dap/data/adapters.lua
@@ -0,0 +1,19 @@
+local M = {}
+
+---Returns the dap config record
+---@param main JavaDebugResolveMainClassRecord
+---@return java-dap.DapLauncherConfig
+function M.main_to_dap_launch_config(main)
+	local project_name = main.projectName
+	local main_class = main.mainClass
+
+	return {
+		request = 'launch',
+		type = 'java',
+		name = string.format('%s -> %s', project_name, main_class),
+		projectName = project_name,
+		mainClass = main_class,
+	}
+end
+
+return M
